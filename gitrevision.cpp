39532839ab111c726913cb673f473b53202f7b55
yy <yaoyuan0616@163.com>
2019-12-17 13:59:26 +0800

    init

---
+++ gitrevision.cpp
@@ -0,0 +1,11 @@
+#include "gitrevision.hpp"
+
+#define NODE_GIT_REVISION_SHA "fd4c0df0a50b74d6eceba5a772afcbaf10d49cf3"
+#define NODE_GIT_REVISION_UNIX_TIMESTAMP 1574236566
+
+namespace nodegit { 
+
+const char* const node_git_revision_sha = NODE_GIT_REVISION_SHA;
+const uint32_t node_git_revision_unix_timestamp = NODE_GIT_REVISION_UNIX_TIMESTAMP;
+
+} // end namespace node
---
+++ gitrevision.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdint.h>
+
+namespace nodegit{
+extern const char* const node_git_revision_sha;
+extern const uint32_t node_git_revision_unix_timestamp;
+} // end namespace git
